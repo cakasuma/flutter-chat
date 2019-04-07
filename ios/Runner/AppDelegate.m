16e25fcd46363906f2a5e8f6521b8bf54927c1e1
Amam Mustofa <mustofa@binary.com>
2019-04-07 12:20:50 +0800

    add to firebase

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -5,9 +5,10 @@
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-  [GeneratedPluginRegistrant registerWithRegistry:self];
-  // Override point for customization after application launch.
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+      [FIRApp configure];
+      [GeneratedPluginRegistrant registerWithRegistry:self];
+      // Override point for customization after application launch.
+      return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
 @end
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/cupertino.dart';
 
 const String _name = "amam";
+
+final ThemeData kIOSTheme = ThemeData(
+  primarySwatch: Colors.orange,
+  primaryColor: Colors.grey[100],
+  primaryColorBrightness: Brightness.light
+);
+
+final ThemeData kAndroidTheme = ThemeData(
+  primarySwatch: Colors.purple,
+  accentColor: Colors.orangeAccent[400]
+);
 
 void main() {
   runApp(ChatApp());
@@ -12,6 +25,7 @@ class ChatApp extends StatelessWidget {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Chat App',
+        theme: defaultTargetPlatform == TargetPlatform.android ? kAndroidTheme : kIOSTheme,
         home: ChatScreen());
   }
 }
@@ -24,9 +38,13 @@ class ChatScreen extends StatefulWidget {
 class _ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
   final List<ChatMessage> _messages = <ChatMessage>[];
   final TextEditingController _textController = TextEditingController();
+  bool _isComposing = false;
 
   _handleSubmitted(String value) {
     _textController.clear();
+    setState(() {
+      _isComposing = false;
+    });
 
     ChatMessage message = ChatMessage(
       text: value,
@@ -52,16 +70,26 @@ class _ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
                 child: TextField(
                   controller: _textController,
                   onSubmitted: _handleSubmitted,
+                  onChanged: (String text) {
+                    setState(() {
+                      _isComposing = text.length > 0;
+                    });
+                  },
                   decoration:
                       InputDecoration.collapsed(hintText: 'Send message'),
                 ),
               ),
               Container(
                 margin: EdgeInsets.symmetric(horizontal: 4.0),
-                child: IconButton(
+                child: Theme.of(context).platform == TargetPlatform.android ?
+                IconButton(
                   icon: Icon(Icons.send),
-                  onPressed: () => _handleSubmitted(_textController.text),
-                ),
+                  onPressed: _isComposing ? () => _handleSubmitted(_textController.text) : null,
+                ) :
+                CupertinoButton(
+                  child: Text('Send'),
+                  onPressed: _isComposing ? () => _handleSubmitted(_textController.text) : null,
+                )
               )
             ],
           )),
@@ -73,25 +101,32 @@ class _ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
     return Scaffold(
       appBar: AppBar(
         title: Text('Chat App'),
+        elevation: Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
       ),
-      body: Column(
-        children: <Widget>[
-          Flexible(
-            child: ListView.builder(
-              padding: EdgeInsets.all(8.0),
-              reverse: true,
-              itemBuilder: (_, int index) => _messages[index],
-              itemCount: _messages.length,
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Flexible(
+              child: ListView.builder(
+                padding: EdgeInsets.all(8.0),
+                reverse: true,
+                itemBuilder: (_, int index) => _messages[index],
+                itemCount: _messages.length,
+              ),
             ),
-          ),
-          Divider(height: 1.0),
-          Container(
-            decoration: BoxDecoration(
-              color: Theme.of(context).cardColor,
-            ),
-            child: _buildTextComposer(),
-          )
-        ],
+            Divider(height: 1.0),
+            Container(
+              decoration: BoxDecoration(
+                color: Theme.of(context).cardColor,
+              ),
+              child: _buildTextComposer(),
+            )
+          ],
+        ),
+        decoration: Theme.of(context).platform == TargetPlatform.android ? null : 
+        BoxDecoration(
+          border: Border(top: BorderSide(color: Colors.grey[200]))
+        ),
       ),
     );
   }
@@ -125,18 +160,20 @@ class ChatMessage extends StatelessWidget {
                 margin: EdgeInsets.only(right: 16.0),
                 child: CircleAvatar(child: Text(_name[0])),
               ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    _name,
-                    style: Theme.of(context).textTheme.subhead,
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(top: 5.0),
-                    child: Text(text),
-                  )
-                ],
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      _name,
+                      style: Theme.of(context).textTheme.subhead,
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 5.0),
+                      child: Text(text),
+                    )
+                  ],
+                ),
               )
             ],
           ),
